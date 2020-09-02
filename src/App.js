45cfcabbee5767e93c4f62e41c0993873e1d8990
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-02 23:05:22 +0900

    Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+/*
+function Food(props) {
+  return <h1>{props.fav}</h1>;
+} */
+function Food({fav}) {
+  return <h1>{fav}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      asdf
+      <h1>asdfasdf</h1>
+      <Food fav="kimchi" />
     </div>
   );
 }
