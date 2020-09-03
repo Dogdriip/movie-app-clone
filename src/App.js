7fef368b0845c9b3dc4e4dcb580306c472f8f29c
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-03 16:06:51 +0900

    map components, prop-types

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-/*
+/* alertnative
 function Food(props) {
   return <h1>{props.fav}</h1>;
 } */
-function Food({fav}) {
-  return <h1>{fav}</h1>;
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h3>Rating: {rating}/5.0</h3>
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: "name1",
+    rating: 4.0,
+  },
+  {
+    id: 2,
+    name: "name2",
+    rating: 2.3,
+  },
+  {
+    id: 3,
+    name: "name3",
+    rating: 4.5,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>asdfasdf</h1>
-      <Food fav="kimchi" />
+      <h1>Hello!</h1>
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} rating={dish.rating} />
+      ))}
     </div>
   );
 }
