74b8e83247403a17acdf000620b36c65bedb9546
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-05 16:35:52 +0900

    Fetching data w/ axios, async/await, etc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    // const res = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // const movies = res.data.data.movies;
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+
+    // this.setState({ movies: movies, isLoading: false });
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false, movies: [] });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <>
-        <h1>{isLoading ? "Loading..." : "Ready!"}</h1>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              // console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
       </>
     );
   }
