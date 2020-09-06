146009d620896f52f8359b98eb1186b5a5a5397d
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-06 11:24:39 +0900

    Add genres to props, configure PropTypes

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
@@ -10,6 +10,13 @@ function Movie({ id, year, title, summary, poster }) {
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <p className="movie__p">{summary}</p>
+        <ul className="movie__genre">
+          {genres.map((genre, idx) => (
+            <li key={idx} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
@@ -21,6 +28,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
