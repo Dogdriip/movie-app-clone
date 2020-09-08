aae42b55c207314b5351346e016b15b063980c05
Hyunseung Jeon <dogdriip@gmail.com>
2020-09-08 16:54:18 +0900

    New component: Detail. Pass props to /movie-detail

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <p className="movie__p">{summary}</p>
-        <ul className="movie__genre">
-          {genres.map((genre, idx) => (
-            <li key={idx} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <p className="movie__p">{summary}</p>
+          <ul className="movie__genre">
+            {genres.map((genre, idx) => (
+              <li key={idx} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
